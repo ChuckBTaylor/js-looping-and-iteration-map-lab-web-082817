2e0abddec81efbfa713b838304ae4a1e6ed36fea
ChuckBTaylor <WLScripps@gmail.com>
2017-10-09 16:43:37 -0400

    Done.

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function lowerCaseDrivers(drivers) {
   return drivers.map(driver => driver.toLowerCase());
 }
 
-function nameToAttributes(drivers) {
-  return drivers.map(function(driver) {
-    const array = driver.split(" ");
-    const obj = { firstName: array[0], lastName: array[1] };
-    return obj;
-  });
-}
 function attributesToPhrase(drivers) {
   return drivers.map(driver => `${driver.name} is from ${driver.hometown}`);
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(driver => ({
+    firstName: driver.split(" ")[0],
+    lastName: driver.split(" ")[1]
+  }));
 }
